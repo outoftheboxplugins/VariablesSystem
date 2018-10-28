02ead6ccaeaeaafd90c005e9f00b836e2b2283bb
pasotee <oalexprea@yahoo.com>
2018-10-28 09:56:01 +0200

    Headers + platforms update

--- Source/VarSystem/Private/FVectorVariable.cpp
+++ Source/VarSystem/Private/FVectorVariable.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Alexandru pasotee Oprea 2018. All Rights Reserved.
 
 #include "FVectorVariable.h"
 
--- Source/VarSystem/Private/VarSystemModule.cpp
+++ Source/VarSystem/Private/VarSystemModule.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+// Copyright Alexandru pasotee Oprea 2018. All Rights Reserved.
 
 #include "Modules/ModuleInterface.h"
 #include "Modules/ModuleManager.h"
