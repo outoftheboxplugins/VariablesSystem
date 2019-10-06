b10e201a6122c9b52c911fb60212743b09472845
pasotee <oalexprea@yahoo.com>
2019-10-06 15:20:07 +0300

    Added save/load for global variables

--- Source/VarSystem/Private/VarSystemModule.cpp
+++ Source/VarSystem/Private/VarSystemModule.cpp
@@ -2,25 +2,81 @@
 
 #include "Modules/ModuleInterface.h"
 #include "Modules/ModuleManager.h"
-
+#include "Engine/World.h"
+#include "BaseVariable.h"
+#include "AssetRegistryModule.h"
 
 /**
  * Implements the VarSystem module.
  */
 class FVarSystemModule
-	: public IModuleInterface
+    : public IModuleInterface
 {
 public:
 
-	//~ IModuleInterface interface
+    //~ IModuleInterface interface
 
-	virtual void StartupModule() override { }
-	virtual void ShutdownModule() override { }
+    virtual void StartupModule() override
+    {
+        FWorldDelegates::OnWorldInitializedActors.AddRaw(this, &FVarSystemModule::OnWorldCreationEvent);
+        FWorldDelegates::OnPostWorldCleanup.AddRaw(this, &FVarSystemModule::OnWorldDestructionEvent);
+    }
+    virtual void ShutdownModule() override
+    {
+        FWorldDelegates::OnWorldInitializedActors.RemoveAll(this);
+        FWorldDelegates::OnPostWorldCleanup.RemoveAll(this);
+    }
 
-	virtual bool SupportsDynamicReloading() override
-	{
-		return true;
-	}
+    virtual bool SupportsDynamicReloading() override
+    {
+        return true;
+    }
+
+private:
+    
+    void OnWorldCreationEvent(const UWorld::FActorsInitializedParams& params)
+    {
+        TArray<UBaseVariable*> variables = GetAllVariables();
+        for (UBaseVariable* variable : variables)
+        {
+            if (variable->ShouldLoad())
+            {
+                variable->Load();
+            }
+        }
+    }
+
+    void OnWorldDestructionEvent(UWorld* World, bool bSessionEnded, bool bCleanupResources)
+    {
+        TArray<UBaseVariable*> variables = GetAllVariables();
+        for (UBaseVariable* variable : variables)
+        {
+            if (variable->ShouldSave())
+            {
+                variable->Save();
+            }
+        }
+    }
+
+    TArray<UBaseVariable*> GetAllVariables() const
+    {
+        TArray<UBaseVariable*> Variables;
+        
+        if (FModuleManager::Get().IsModuleLoaded("AssetRegistry"))
+        {
+            FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+            TArray<FAssetData> AssetData;
+            AssetRegistryModule.Get().GetAssetsByClass(FName("BaseVariable"), AssetData);
+            for (int i = 0; i < AssetData.Num(); i++) {
+                UBaseVariable* VariableFound = Cast<UBaseVariable>(AssetData[i].GetAsset());
+                if (VariableFound != NULL) {
+                    Variables.Add(VariableFound);
+                }
+            }
+        }
+        
+        return Variables;
+    }
 };
 
 
--- Source/VarSystem/Public/BaseVariable.h
+++ Source/VarSystem/Public/BaseVariable.h
@@ -7,6 +7,15 @@
 #include "UObject/ObjectMacros.h"
 #include "BaseVariable.generated.h"
 
+UENUM(BlueprintType)
+enum class EVarSystemSaveType : uint8
+{
+    VSST_None                  UMETA(DisplayName = "None"),
+    VSST_LoadOnStart 	       UMETA(DisplayName = "LoadOnStart"),
+    VSST_SaveOnFinish 	       UMETA(DisplayName = "SaveOnFinish"),
+    VSST_StartAndFinish	       UMETA(DisplayName = "StartAndFinish")
+};
+
 /**
  * Implements an asset that can store specific types of variables.
  */
@@ -16,15 +25,22 @@ class VARSYSTEM_API UBaseVariable : public USaveGame
 	GENERATED_BODY()
 
 public:
-	/** Full description of the variable usage.. */
+	/** Full description of the variable usage. */
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category="VarSystem")
 	FText VariableDescription;
+
+    /** Desired save-load behavior of the variable. */
+    UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "VarSystem")
+    EVarSystemSaveType SaveBehavior = EVarSystemSaveType::VSST_StartAndFinish;
 
 	/* Saves the current data of a variable.*/
 	virtual void Save() {};
 
 	/* Loads the data of a variable and assignees it. */
 	virtual void Load() {};
+
+    bool ShouldSave() const { return SaveBehavior == EVarSystemSaveType::VSST_SaveOnFinish || SaveBehavior == EVarSystemSaveType::VSST_StartAndFinish; }
+    bool ShouldLoad() const { return SaveBehavior == EVarSystemSaveType::VSST_LoadOnStart || SaveBehavior == EVarSystemSaveType::VSST_StartAndFinish; }
 
 protected:
 	bool dirty = false;
