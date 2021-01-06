4e81fdca47410a546cc79747f37759b0a9d451f9
pasotee <pasotee.alexandru@gmail.com>
2021-01-06 21:21:21 +0200

    Added prebuild steps

--- Generator/VariablesGenerator/Generated/Node/CreateSpecificNodeGet.h
+++ Generator/VariablesGenerator/Generated/Node/CreateSpecificNodeGet.h
@@ -1,72 +1,72 @@
     if (VariableClassName == FName("GlobalObjectRefVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalObjectRefVariable, GetGlobalObjectRefVariableValue), UGlobalObjectRefVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalObjectRefVariable, GetGlobalObjectRefVariableValue), UGlobalObjectRefVariable::StaticClass());
     }
     if (VariableClassName == FName("InstancedObjectRefVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedObjectRefVariable, GetInstancedObjectRefVariableValue), UInstancedObjectRefVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedObjectRefVariable, GetInstancedObjectRefVariableValue), UInstancedObjectRefVariable::StaticClass());
     }
     if (VariableClassName == FName("GlobalActorRefVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalActorRefVariable, GetGlobalActorRefVariableValue), UGlobalActorRefVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalActorRefVariable, GetGlobalActorRefVariableValue), UGlobalActorRefVariable::StaticClass());
     }
     if (VariableClassName == FName("InstancedActorRefVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedActorRefVariable, GetInstancedActorRefVariableValue), UInstancedActorRefVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedActorRefVariable, GetInstancedActorRefVariableValue), UInstancedActorRefVariable::StaticClass());
     }
     if (VariableClassName == FName("GlobalBoolVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalBoolVariable, GetGlobalBoolVariableValue), UGlobalBoolVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalBoolVariable, GetGlobalBoolVariableValue), UGlobalBoolVariable::StaticClass());
     }
     if (VariableClassName == FName("InstancedBoolVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedBoolVariable, GetInstancedBoolVariableValue), UInstancedBoolVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedBoolVariable, GetInstancedBoolVariableValue), UInstancedBoolVariable::StaticClass());
     }
     if (VariableClassName == FName("GlobalIntVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalIntVariable, GetGlobalIntVariableValue), UGlobalIntVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalIntVariable, GetGlobalIntVariableValue), UGlobalIntVariable::StaticClass());
     }
     if (VariableClassName == FName("InstancedIntVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedIntVariable, GetInstancedIntVariableValue), UInstancedIntVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedIntVariable, GetInstancedIntVariableValue), UInstancedIntVariable::StaticClass());
     }
     if (VariableClassName == FName("GlobalFloatVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFloatVariable, GetGlobalFloatVariableValue), UGlobalFloatVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFloatVariable, GetGlobalFloatVariableValue), UGlobalFloatVariable::StaticClass());
     }
     if (VariableClassName == FName("InstancedFloatVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFloatVariable, GetInstancedFloatVariableValue), UInstancedFloatVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFloatVariable, GetInstancedFloatVariableValue), UInstancedFloatVariable::StaticClass());
     }
     if (VariableClassName == FName("GlobalStringVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalStringVariable, GetGlobalStringVariableValue), UGlobalStringVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalStringVariable, GetGlobalStringVariableValue), UGlobalStringVariable::StaticClass());
     }
     if (VariableClassName == FName("InstancedStringVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedStringVariable, GetInstancedStringVariableValue), UInstancedStringVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedStringVariable, GetInstancedStringVariableValue), UInstancedStringVariable::StaticClass());
     }
     if (VariableClassName == FName("GlobalFRotatorVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFRotatorVariable, GetGlobalFRotatorVariableValue), UGlobalFRotatorVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFRotatorVariable, GetGlobalFRotatorVariableValue), UGlobalFRotatorVariable::StaticClass());
     }
     if (VariableClassName == FName("InstancedFRotatorVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFRotatorVariable, GetInstancedFRotatorVariableValue), UInstancedFRotatorVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFRotatorVariable, GetInstancedFRotatorVariableValue), UInstancedFRotatorVariable::StaticClass());
     }
     if (VariableClassName == FName("GlobalFVectorVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFVectorVariable, GetGlobalFVectorVariableValue), UGlobalFVectorVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFVectorVariable, GetGlobalFVectorVariableValue), UGlobalFVectorVariable::StaticClass());
     }
     if (VariableClassName == FName("InstancedFVectorVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFVectorVariable, GetInstancedFVectorVariableValue), UInstancedFVectorVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFVectorVariable, GetInstancedFVectorVariableValue), UInstancedFVectorVariable::StaticClass());
     }
     if (VariableClassName == FName("GlobalFVector2DVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFVector2DVariable, GetGlobalFVector2DVariableValue), UGlobalFVector2DVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFVector2DVariable, GetGlobalFVector2DVariableValue), UGlobalFVector2DVariable::StaticClass());
     }
     if (VariableClassName == FName("InstancedFVector2DVariable"))
     {
-        resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFVector2DVariable, GetInstancedFVector2DVariableValue), UInstancedFVector2DVariable::StaticClass());
+        ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFVector2DVariable, GetInstancedFVector2DVariableValue), UInstancedFVector2DVariable::StaticClass());
     }
--- Source/VariablesSystem/Generated/Node/CreateSpecificNodeSet.h
+++ Source/VariablesSystem/Generated/Node/CreateSpecificNodeSet.h
@@ -1,81 +1,81 @@
         if (VariableClassName == FName("GlobalObjectRefVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalObjectRefVariable, SetGlobalObjectRefVariableValue), UGlobalObjectRefVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalObjectRefVariable, SetGlobalObjectRefVariableValue), UGlobalObjectRefVariable::StaticClass());
         }
 
         if (VariableClassName == FName("InstancedObjectRefVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedObjectRefVariable, SetInstancedObjectRefVariableValue), UInstancedObjectRefVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedObjectRefVariable, SetInstancedObjectRefVariableValue), UInstancedObjectRefVariable::StaticClass());
         }
         if (VariableClassName == FName("GlobalActorRefVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalActorRefVariable, SetGlobalActorRefVariableValue), UGlobalActorRefVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalActorRefVariable, SetGlobalActorRefVariableValue), UGlobalActorRefVariable::StaticClass());
         }
 
         if (VariableClassName == FName("InstancedActorRefVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedActorRefVariable, SetInstancedActorRefVariableValue), UInstancedActorRefVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedActorRefVariable, SetInstancedActorRefVariableValue), UInstancedActorRefVariable::StaticClass());
         }
         if (VariableClassName == FName("GlobalBoolVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalBoolVariable, SetGlobalBoolVariableValue), UGlobalBoolVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalBoolVariable, SetGlobalBoolVariableValue), UGlobalBoolVariable::StaticClass());
         }
 
         if (VariableClassName == FName("InstancedBoolVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedBoolVariable, SetInstancedBoolVariableValue), UInstancedBoolVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedBoolVariable, SetInstancedBoolVariableValue), UInstancedBoolVariable::StaticClass());
         }
         if (VariableClassName == FName("GlobalIntVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalIntVariable, SetGlobalIntVariableValue), UGlobalIntVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalIntVariable, SetGlobalIntVariableValue), UGlobalIntVariable::StaticClass());
         }
 
         if (VariableClassName == FName("InstancedIntVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedIntVariable, SetInstancedIntVariableValue), UInstancedIntVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedIntVariable, SetInstancedIntVariableValue), UInstancedIntVariable::StaticClass());
         }
         if (VariableClassName == FName("GlobalFloatVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFloatVariable, SetGlobalFloatVariableValue), UGlobalFloatVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFloatVariable, SetGlobalFloatVariableValue), UGlobalFloatVariable::StaticClass());
         }
 
         if (VariableClassName == FName("InstancedFloatVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFloatVariable, SetInstancedFloatVariableValue), UInstancedFloatVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFloatVariable, SetInstancedFloatVariableValue), UInstancedFloatVariable::StaticClass());
         }
         if (VariableClassName == FName("GlobalStringVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalStringVariable, SetGlobalStringVariableValue), UGlobalStringVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalStringVariable, SetGlobalStringVariableValue), UGlobalStringVariable::StaticClass());
         }
 
         if (VariableClassName == FName("InstancedStringVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedStringVariable, SetInstancedStringVariableValue), UInstancedStringVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedStringVariable, SetInstancedStringVariableValue), UInstancedStringVariable::StaticClass());
         }
         if (VariableClassName == FName("GlobalFRotatorVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFRotatorVariable, SetGlobalFRotatorVariableValue), UGlobalFRotatorVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFRotatorVariable, SetGlobalFRotatorVariableValue), UGlobalFRotatorVariable::StaticClass());
         }
 
         if (VariableClassName == FName("InstancedFRotatorVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFRotatorVariable, SetInstancedFRotatorVariableValue), UInstancedFRotatorVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFRotatorVariable, SetInstancedFRotatorVariableValue), UInstancedFRotatorVariable::StaticClass());
         }
         if (VariableClassName == FName("GlobalFVectorVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFVectorVariable, SetGlobalFVectorVariableValue), UGlobalFVectorVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFVectorVariable, SetGlobalFVectorVariableValue), UGlobalFVectorVariable::StaticClass());
         }
 
         if (VariableClassName == FName("InstancedFVectorVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFVectorVariable, SetInstancedFVectorVariableValue), UInstancedFVectorVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFVectorVariable, SetInstancedFVectorVariableValue), UInstancedFVectorVariable::StaticClass());
         }
         if (VariableClassName == FName("GlobalFVector2DVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFVector2DVariable, SetGlobalFVector2DVariableValue), UGlobalFVector2DVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UGlobalFVector2DVariable, SetGlobalFVector2DVariableValue), UGlobalFVector2DVariable::StaticClass());
         }
 
         if (VariableClassName == FName("InstancedFVector2DVariable"))
         {
-            resultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFVector2DVariable, SetInstancedFVector2DVariableValue), UInstancedFVector2DVariable::StaticClass());
+            ResultCreateNode->FunctionReference.SetExternalMember(GET_FUNCTION_NAME_CHECKED(UInstancedFVector2DVariable, SetInstancedFVector2DVariableValue), UInstancedFVector2DVariable::StaticClass());
         }
