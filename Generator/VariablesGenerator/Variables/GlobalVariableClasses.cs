9133beb8dbc139a8b84cd89cf9fdcbf0479e8af3
Alexandru Oprea <oalexprea@yahoo.com>
2021-07-03 21:28:46 +0300

    enabled array variations

--- Generator/VariablesGenerator/Variables/GlobalVariableClasses.cs
+++ Generator/VariablesGenerator/Variables/GlobalVariableClasses.cs
@@ -10,37 +10,37 @@ public class ObjectRefVariable : BaseVariable { }
 public class ActorRefVariable : BaseVariable { }
 
 [GlobalVar("bool", "false", "Boolean", "", "None", "Boolean")]
-[GenerateExtra(generateSaveLoad = true, generateArray = false)]
+[GenerateExtra(generateSaveLoad = true, generateArray = true)]
 [DebugInfo("UKismetStringLibrary::Conv_BoolToString(Item)")]
 public class BoolVariable : BaseVariable { }
 
 [GlobalVar("int32", "0", "Int", "", "None", "Int")]
-[GenerateExtra(generateSaveLoad = true, generateArray = false)]
+[GenerateExtra(generateSaveLoad = true, generateArray = true)]
 [DebugInfo("UKismetStringLibrary::Conv_IntToString(Item)")]
 public class IntVariable : BaseVariable { }
 
 [GlobalVar("float", "0.0f", "Float", "", "None", "Float")]
-[GenerateExtra(generateSaveLoad = true, generateArray = false)]
+[GenerateExtra(generateSaveLoad = true, generateArray = true)]
 [DebugInfo("UKismetStringLibrary::Conv_FloatToString(Item)")]
 public class FloatVariable : BaseVariable { }
 
 [GlobalVar("FString", "\"\"", "String", "", "None", "String")]
-[GenerateExtra(generateSaveLoad = true, generateArray = false)]
+[GenerateExtra(generateSaveLoad = true, generateArray = true)]
 [DebugInfo("Item")]
 public class StringVariable : BaseVariable { }
 
 [GlobalVar("FRotator", "FRotator::ZeroRotator", "Struct", "TBaseStructure<FRotator>::Get()", "None", "Rotator")]
-[GenerateExtra(generateSaveLoad = true, generateArray = false)]
+[GenerateExtra(generateSaveLoad = true, generateArray = true)]
 [DebugInfo("UKismetStringLibrary::Conv_RotatorToString(Item)")]
 public class FRotatorVariable : BaseVariable { }
 
 [GlobalVar("FVector", "FVector::ZeroVector", "Struct", "TBaseStructure<FVector>::Get()", "None", "Vector")]
-[GenerateExtra(generateSaveLoad = true, generateArray = false)]
+[GenerateExtra(generateSaveLoad = true, generateArray = true)]
 [DebugInfo("UKismetStringLibrary::Conv_VectorToString(Item)")]
 public class FVectorVariable : BaseVariable { }
 
 [GlobalVar("FVector2D", "FVector2D::ZeroVector", "Struct", "TBaseStructure<FVector2D>::Get()", "None", "Vector")]
-[GenerateExtra(generateSaveLoad = true, generateArray = false)]
+[GenerateExtra(generateSaveLoad = true, generateArray = true)]
 [DebugInfo("UKismetStringLibrary::Conv_Vector2dToString(Item)")]
 public class FVector2DVariable : BaseVariable { }
 
