1baba6771cd5bf0cf24521ff747c4d313a6eae82
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-11-05 14:17:00 +0200

    copying over the datatable model

---
+++ Source/VariablesSystem/Public/GlobalCustomVariable.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+
+#include "GlobalCustomVariable.generated.h"
+
+/**
+ *
+ */
+UCLASS(abstract, Blueprintable)
+class VARIABLESSYSTEM_API UGlobalCustomVariable : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, Category = DataTable, meta = (DisplayThumbnail = "false"))
+	TObjectPtr<UScriptStruct> RowStruct;
+};
---
+++ Source/VariablesSystemEditor/Private/Factories/VSCustomFactory.h
@@ -0,0 +1,29 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Factories/Factory.h"
+#include "UObject/ObjectMacros.h"
+
+#include "VSCustomFactory.generated.h"
+
+class UGlobalCustomVariable;
+
+UCLASS(hidecategories = Object)
+class UVSCustomFactory : public UFactory
+{
+	GENERATED_UCLASS_BODY()
+
+	UPROPERTY(BlueprintReadWrite, Category = "Data Table Factory")
+	TObjectPtr<const class UScriptStruct> Struct;
+
+	//~ Begin UFactory Interface
+	virtual bool ConfigureProperties() override;
+	virtual UObject* FactoryCreateNew(
+		UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
+	//~ Begin UFactory Interface
+
+protected:
+	virtual UGlobalCustomVariable* MakeNewDataTable(UObject* InParent, FName Name, EObjectFlags Flags);
+};
