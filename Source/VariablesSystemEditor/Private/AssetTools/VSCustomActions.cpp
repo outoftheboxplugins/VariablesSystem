eb6f73f2a36415c1c64c566828dd3747a57d47a1
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-11-05 14:33:12 +0200

    added actions to enable factory

---
+++ Source/VariablesSystemEditor/Private/AssetTools/VSCustomActions.cpp
@@ -0,0 +1,41 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2021. All Rights Reserved.
+
+#include "VSCustomActions.h"
+
+#include "GlobalCustomVariable.h"
+
+#define LOCTEXT_NAMESPACE "AutomationWizard"
+
+namespace
+{
+FName CustomAssetCategoryRegisterName = FName(TEXT("OutOfTheBox"));
+FText CustomAssetCategoryDisplayName = LOCTEXT("OutOfTheBoxCategory", "OutOfTheBox");
+}	 // namespace
+
+FVSCustomActions::FVSCustomActions()
+{
+	IAssetTools& AssetTools = FAssetToolsModule::GetModule().Get();
+	AssetCategoryBit = AssetTools.RegisterAdvancedAssetCategory(CustomAssetCategoryRegisterName, CustomAssetCategoryDisplayName);
+}
+
+FText FVSCustomActions::GetName() const
+{
+	return LOCTEXT("AssetName", "Automation Command");
+}
+
+FColor FVSCustomActions::GetTypeColor() const
+{
+	return FColor(183, 83, 183);
+}
+
+uint32 FVSCustomActions::GetCategories()
+{
+	return AssetCategoryBit;
+}
+
+UClass* FVSCustomActions::GetSupportedClass() const
+{
+	return UGlobalCustomVariable::StaticClass();
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/VariablesSystemEditor/Private/AssetTools/VSCustomActions.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "AssetTypeActions_Base.h"
+
+/**
+ * Asset actions for UAwCommands.
+ */
+
+class FVSCustomActions : public FAssetTypeActions_Base
+{
+public:
+	FVSCustomActions();
+
+	// FAssetTypeActions_Base interface
+private:
+	virtual FText GetName() const override;
+	virtual FColor GetTypeColor() const override;
+	virtual uint32 GetCategories() override;
+	virtual UClass* GetSupportedClass() const override;
+
+private:
+	EAssetTypeCategories::Type AssetCategoryBit;
+};
