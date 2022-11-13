c05e842b69e4f0ef3c4f942b4a8a52759197f3e4
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-11-13 21:10:17 +0200

    fixed compilation for ue5.1

--- Source/VariablesSystemEditor/Private/Factories/VSCustomFactory.cpp
+++ Source/VariablesSystemEditor/Private/Factories/VSCustomFactory.cpp
@@ -37,8 +37,8 @@ bool UVSCustomFactory::ConfigureProperties()
 			return FDataTableEditorUtils::IsValidTableStruct(InStruct);
 		}
 
-		virtual bool IsUnloadedStructAllowed(const FStructViewerInitializationOptions& InInitOptions, const FName InStructPath,
-			TSharedRef<FStructViewerFilterFuncs> InFilterFuncs) override
+		virtual bool IsUnloadedStructAllowed(const FStructViewerInitializationOptions& InInitOptions,
+			const FSoftObjectPath& InStructPath, TSharedRef<class FStructViewerFilterFuncs> InFilterFuncs) override
 		{
 			// Unloaded structs are always User Defined Structs, and User Defined Structs are always allowed
 			// They will be re-validated by IsStructAllowed once loaded during the pick
