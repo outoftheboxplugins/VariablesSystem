6aa6db97260316215ce6735edad01403668ebd56
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-05-12 18:17:36 +0300

    fixing includes & applying style

--- Source/VariablesSystem/Public/VariablesSystemHelpers.h
+++ Source/VariablesSystem/Public/VariablesSystemHelpers.h
@@ -1,11 +1,10 @@
 // Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
-#pragma once 
+#pragma once
 
+#include "AssetRegistry/AssetData.h"
+#include "AssetRegistry/AssetRegistryModule.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
-
-#include "AssetRegistryModule.h"
 #include "Modules/ModuleManager.h"
-
 #include "VSLog.h"
 
 #include "VariablesSystemHelpers.generated.h"
@@ -21,18 +20,18 @@ UCLASS(meta = (BlueprintThreadSafe))
 class VARIABLESSYSTEM_API UVariablesSystemHelpersBPLibrary : public UBlueprintFunctionLibrary
 {
 public:
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 	// Returns all assets of a certain type from the content browser.
-	template<class T>
+	template <class T>
 	static TArray<T*> GetAllAssetsOfType();
 
-    // Returns all the variables (UObjects) assets from the content browser.
-    static TArray<UBaseVariable*> GetAllVariables();
+	// Returns all the variables (UObjects) assets from the content browser.
+	static TArray<UBaseVariable*> GetAllVariables();
 
 	// Returns all the variables (UObjects) assets from the content browser.
 	static TArray<UGlobalVariable*> GetAllGlobalVariables();
-    
+
 	// Triggers a Save operation on all the Variable Assets
 	UFUNCTION(BlueprintCallable, Category = "VariablesSystem")
 	static void SaveAllVariables();
@@ -42,34 +41,35 @@ public:
 	static void LoadAllVariables();
 };
 
-template<class T>
+template <class T>
 TArray<T*> UVariablesSystemHelpersBPLibrary::GetAllAssetsOfType()
 {
-    TArray<T*> Assets;
+	TArray<T*> Assets;
 
-    if (FModuleManager::Get().IsModuleLoaded("AssetRegistry"))
-    {
-        FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
-        TArray<FAssetData> assetDatas;
+	if (FModuleManager::Get().IsModuleLoaded("AssetRegistry"))
+	{
+		FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+		TArray<FAssetData> AssetDatas;
 
-        FARFilter SearchFilter;
-        SearchFilter.ClassPaths.Add(T::StaticClass()->GetClassPathName());
-        SearchFilter.bRecursiveClasses = true;
+		FARFilter SearchFilter;
+		SearchFilter.ClassPaths.Add(T::StaticClass()->GetClassPathName());
+		SearchFilter.bRecursiveClasses = true;
 
-        AssetRegistryModule.Get().GetAssets(SearchFilter, assetDatas);
+		AssetRegistryModule.Get().GetAssets(SearchFilter, AssetDatas);
 
-        for (auto& assetData : assetDatas)
-        {
-            T* VariableFound = Cast<T>(assetData.GetAsset());
-            if (VariableFound != NULL) {
-                Assets.Add(VariableFound);
-            }
-        }
-    }
+		for (auto& AssetData : AssetDatas)
+		{
+			T* VariableFound = Cast<T>(AssetData.GetAsset());
+			if (VariableFound != nullptr)
+			{
+				Assets.Add(VariableFound);
+			}
+		}
+	}
 	else
 	{
 		UE_LOG(LogVariablesSystem, Error, TEXT("AssetRegistry Module not loaded!"));
 	}
 
-    return Assets;
+	return Assets;
 }
