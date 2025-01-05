4d9ebf4b675e9e04623da8d2b0e82b25f087eefb
Alexandru Oprea <oprea_alexandrum@yahoo.com>
2025-01-05 19:44:23 +0200

    Compiler fix for logger

--- Source/VariablesSystem/Public/VSLog.h
+++ Source/VariablesSystem/Public/VSLog.h
@@ -1,6 +1,8 @@
 // Copyright Out-of-the-Box Plugins 2018-2025. All Rights Reserved.
 #pragma once
 
+#include "CoreMinimal.h"
+
 VARIABLESSYSTEM_API DECLARE_LOG_CATEGORY_EXTERN(LogVariablesSystem, Log, All);
 
 #define LOG_TRACE() UE_LOG(LogVariablesSystem, Log, TEXT("Trace"))
